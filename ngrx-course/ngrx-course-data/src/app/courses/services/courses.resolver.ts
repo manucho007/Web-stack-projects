0edd8de14e2d8ab7f425503c22152c146200e6da
Manuel Rodriguez <manrodri92@gmail.com>
2021-03-11 10:56:11 +0300

    Completed resolver for courses entity

--- ngrx-course/ngrx-course-data/src/app/courses/services/courses.resolver.ts
+++ ngrx-course/ngrx-course-data/src/app/courses/services/courses.resolver.ts
@@ -5,7 +5,7 @@ import {
   RouterStateSnapshot,
 } from "@angular/router";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { filter, first, map, tap } from "rxjs/operators";
 import { CourseEntityService } from "./course-entity.service";
 
 @Injectable()
@@ -17,7 +17,16 @@ export class CoursesResolver implements Resolve<boolean> {
     state: RouterStateSnapshot
   ): Observable<boolean> {
     //   This get all triggers a get request automatically by convention, there is no need to do a request manually now
-    // There are many many more methods available with this
-    return this.coursesService.getAll().pipe(map((courses) => !!courses));
+    // There are many many more methods available with this but get all should not fetch the data just trigger an http request
+    return this.coursesService.loaded$.pipe(
+      // The loaded cache we're getting is in the entity, and based on this we'll tigger an http response to get the data
+      tap((loaded) => {
+        if (!loaded) {
+          this.coursesService.getAll();
+        }
+      }),
+      filter((loaded) => !!loaded),
+      first()
+    );
   }
 }
